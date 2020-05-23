92bde3d1af145406441f8a8185d9a44258871aae
Rico Brase <kontakt@rico-brase.de>
2020-05-23 13:53:56 +0200

    Bumped version to 1.3.0

--- src/main/java/dev/ricobrase/chatcalculator/ChatCalculatorMod.java
+++ src/main/java/dev/ricobrase/chatcalculator/ChatCalculatorMod.java
@@ -11,7 +11,7 @@ public class ChatCalculatorMod {
 
     public static final String NAME = "ChatCalculator";
     public static final String MODID = "chatcalculator";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
     public static final String MC_VERSION = "[1.12.2]";
 
     public static Logger logger;
